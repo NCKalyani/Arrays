7297b4e63f044ab56b977c20e098de681c5e8ee8
kalyani <n.chandra.kalyani@gmail.com>
2024-03-19 17:53:21 +0530

    code

--- lists/reverse.py
+++ lists/reverse.py
@@ -1,2 +1,15 @@
 def reverse_list(li)->list:
+    # initialise i,j
+    i = 0
+    j = len(li) - 1
+    # loop runs until mid_length 
+    while i < j:
+        #swap list
+        temp = li[i]
+        li[i] = li[j]
+        li[j] = temp
+        #modify variables 
+        i += 1
+        j -= 1
     
+    return li    
