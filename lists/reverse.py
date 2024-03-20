0c954d17c95fcdec20abff6df9e4ba766917e628
kalyani <n.chandra.kalyani@gmail.com>
2024-03-20 10:34:03 +0530

    Adding docstrings

--- lists/reverse.py
+++ lists/reverse.py
@@ -1,15 +1,16 @@
-def reverse_list(li)->list:
+def reverse_list(li: list) -> list:
+    """Takes list as an argument and gives reverse of list as output"""
     # initialise i,j
     i = 0
     j = len(li) - 1
     # loop runs until mid_length 
     while i < j:
-        #swap list
+        # swap list
         temp = li[i]
         li[i] = li[j]
         li[j] = temp
-        #modify variables 
+        # modify variables to avoid infinite loop
         i += 1
         j -= 1
-    
-    return li    
+
+    return li
