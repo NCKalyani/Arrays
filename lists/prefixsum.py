14a48bae9691b597cfe1952fd6acb404b94c527f
kalyani <n.chandra.kalyani@gmail.com>
2024-03-20 19:13:24 +0530

    included range sum function

--- lists/prefixsum.py
+++ lists/prefixsum.py
@@ -6,3 +6,20 @@ def prefix_sum(a: list) -> list:
         ps.append(a[i]) if i == 0 else ps.append(ps[i - 1] + a[i])
 
     return ps
+
+
+def range_sum(a: list, b: list[list]) -> list:
+    """ Returns sum of list in the given range """
+    result = []
+    for i in b:
+        s, e = i
+        ps = prefix_sum(a)
+        if s == 0:
+            psum = ps[e]
+        else:
+            psum = ps[e] - ps[s - 1]
+        result.append(psum)
+    return result
+
+
+# print(range_sum([1, 2, 3, 4, 5], [[0, 3], [1, 2]]))
